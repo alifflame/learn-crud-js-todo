d6864efff6f9e28388a36a910bd42393638e705b
Alif Aunurrohim <alifaunurrohim@gmail.com>
2022-05-24 16:44:33 +0700

    complete to do scratch web example

--- main.js
+++ main.js
@@ -32,12 +32,21 @@ let acceptData = () => {
 let createPost = () => {
     posts.innerHTML += `
     <div>
-        <p>${data.text};</p>
+        <p>${data.text}</p>
         <span class="options">
-            <i class="fas fa-edit"></i>
-            <i class="fas fa-trash-alt"></i>
+            <i onClick="editPost(this)" class="fas fa-edit"></i>
+            <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
         </span>
     </div>
     `;
+    input.value = '';
+};
 
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+    input.value = e.parentElement.parentElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
 };
